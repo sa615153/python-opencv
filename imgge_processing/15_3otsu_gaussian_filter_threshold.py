4ff13b3327670f5efd25a3bd894578e1995db037
Pang Jian Ren <sa615153@mail.ustc.edu.cn>
2017-02-16 08:22:57 +0800

    after threshold

---
+++ imgge_processing/15_3otsu_gaussian_filter_threshold.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+img = cv2.imread('noisy2.png',0)
+# global thresholding
+ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# Otsu's thresholding
+ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# Otsu's thresholding after Gaussian filtering
+#（5,5）为高斯核的大小，0 为标准差
+blur = cv2.GaussianBlur(img,(5,5),0)
+# 阈值一定要设为0！
+ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+# plot all the images and their histograms
+images = [img, 0, th1,
+           img, 0, th2,
+            blur, 0, th3]
+titles = ['Original Noisy Image','Histogram','Global Thresholding (v=127)',
+'Original Noisy Image','Histogram',"Otsu's Thresholding",
+'Gaussian filtered Image','Histogram',"Otsu's Thresholding"]
+
+# 这里使用了pyplot 中画直方图的方法，plt.hist, 要注意的是它的参数是一维数组
+# 所以这里使用了（numpy）ravel 方法，将多维数组转换成一维，也可以使用flatten 方法
+#ndarray.flat 1-D iterator over an array.
+#ndarray.flatten 1-D array copy of the elements of an array in row-major order.
+for i in xrange(3):
+    plt.subplot(3,3,i*3+1),plt.imshow(images[i*3],'gray')
+    plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
+    plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
+    plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
+plt.show()
+
+
+
+# how otsu works
+# 两边方差最小
+img = cv2.imread('noisy2.png',0)
+blur = cv2.GaussianBlur(img,(5,5),0)
+
+# find normalized_histogram, and its cumulative distribution function
+# 计算归一化直方图
+# CalcHist(image, accumulate=0, mask=NULL)
+hist = cv2.calcHist([blur],[0],None,[256],[0,256])
+hist_norm = hist.ravel()/hist.max()
+Q = hist_norm.cumsum()
+
+bins = np.arange(256)
+
+fn_min = np.inf
+thresh = -1
+
+for i in xrange(1,256):
+    p1,p2 = np.hsplit(hist_norm,[i]) # probabilities
+    q1,q2 = Q[i],Q[255]-Q[i] # cum sum of classes
+    b1,b2 = np.hsplit(bins,[i]) # weights
+
+    # finding means and variances
+    m1,m2 = np.sum(p1*b1)/q1, np.sum(p2*b2)/q2
+    v1,v2 = np.sum(((b1-m1)**2)*p1)/q1,np.sum(((b2-m2)**2)*p2)/q2
+
+    # calculates the minimization function
+    fn = v1*q1 + v2*q2
+    if fn < fn_min:
+        fn_min = fn
+        thresh = i
+
+# find otsu's threshold value with OpenCV function
+ret, otsu = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+print thresh,ret
